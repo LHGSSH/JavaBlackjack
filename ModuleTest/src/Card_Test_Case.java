92e3058300edacbcb871779e84aebac515b521fb
Chad Schadewald <ces83@students.uwf.edu>
2019-10-19 19:33:07 -0500

    Adding jUnit Test Case

---
+++ ModuleTest/src/Card_Test_Case.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class Card_Test_Case {
+
+    @org.junit.jupiter.api.Test
+    void main()
+    {
+        System.out.println("Testing");
+    }
+}
