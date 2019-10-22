8ecbe7cdd6ea4367394aeae709debdb604b90a19
dmeade99 <dmeade99@gmail.com>
2019-10-22 13:49:23 -0500

    Initial attempt at Shoe.java

---
+++ Shoe.java
@@ -0,0 +1,45 @@
+public class Shoe
+{
+    private Deck deck01;
+    private Deck deck02;
+    private Deck deck03;
+    private Deck deck04;
+    private Deck deck05;
+    private Deck deck06;
+    private Deck deck07;
+    private Deck deck08;
+    private Deck currentDeck;
+    private int decksRemaining;
+
+    public Shoe()
+    {
+        this.deck01 = Deck();
+        this.deck02 = Deck();
+        this.deck03 = Deck();
+        this.deck04 = Deck();
+        this.deck05 = Deck();
+        this.deck06 = Deck();
+        this.deck07 = Deck();
+        this.deck08 = Deck();
+
+        this.currentDeck = this.deck01;
+
+        this.decksRemaining = 8;
+    }
+
+    public isShoeEmpty()
+    {
+        if(decksRemaining >= 1)
+            return FALSE;
+        else
+            return TRUE;
+    }
+
+    public deal(Deck currentDeck)
+    {
+        // Presumed that dealCard method exists within Deck class
+        return currentDeck.dealCard();
+    }
+
+
+}
