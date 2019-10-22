a3867c7c4fadc67c7a9755338446b27487220226
LHGSSH <liamhg1@yahoo.com>
2019-10-22 15:26:16 -0500

    Added comments and first variable to Card.

--- card.java
+++ card.java
@@ -1,1 +1,16 @@
-//Test
+/**
+ * This class represents a playing card.
+ * 
+ * @author Liam Hilton-Green
+ * @version 1.0
+ * 
+ * CEN    Project #: 5
+ * File Name: card.java
+ */
+
+public class Card {
+	/**
+	 * The suit of the card (hearts, spades, etc.).
+	 */
+	private String suit;
+}
