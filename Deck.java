cf306134923e6879b746e3a5b110ab802bbf2771
jdk31 <55603272+jdk31@users.noreply.github.com>
2019-10-25 11:35:05 -0500

    Finished v1 of Deck.java
    Still needs a way to handle when a deck runs out of cards. This may be a responsibility of the game manager class

--- Deck.java
+++ Deck.java
@@ -1,22 +1,48 @@
+/**
+ * This class represents a Deck of Card objects using an ArrayList and contains
+ * methods to shuffle and deal the cards
+ * 
+ * @author Joshua Kleine
+ * @version 1.0
+ * 
+ * CEN3031    Project
+ * File Name: Deck.java
+ */
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.ListIterator;
+
 public class Deck {
 
     private int nextCard;
-    private Card deckOfCards[] = new Card[52];
+    private ArrayList<Card> deckOfCards = new ArrayList<Card>(52);
 
+    //Two loops to create all 52 cards. The inner loop is complicated, but allows for this
+    //code to hold even for changes in the size of a deck or types of suits.
     public Deck(){
-    //Needs to be filled with loop to create deck array
+      for (Suit suit : Suit.values()){
+            for (int i = 1; i <= (deckOfCards.size() / Suit.values().length); i++){
+                Card card = new Card(suit, i);
+                deckOfCards.add(card);
+            }
+      }
     }
     /*
-     This method will be filed, its just here for the UML and so it will compile for now
+     This method mixes the ArrayList of Cards. It was modified to not return the deck.
      */
-    public Deck Shuffle(Deck deck){
-        return deck;
+    public void Shuffle(){
+        Collections.shuffle(deckOfCards);
     }
     /*
-    This method will be filed, its just here for the UML and so it will compile for now
-     */
+    This method Returns the next card object from the deck arraylist, and deletes
+    that element.
+    TODO manage what happens when the iterator tries to go beyond the deck size.
+    (either in this class or in the manager class)
+    */
     public Card dealCard(){
-       Card card = new Card(Suit.HEARTS,3);
-        return card;
+        ListIterator<Card> cardIterator = deckOfCards.listIterator();
+        Card c = cardIterator.next();
+        cardIterator.remove();
+        return c;
     }
 }
