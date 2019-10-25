333059824fae2db0c7e7659e59e611dfd683b0a8
Chad Schadewald <ces83@students.uwf.edu>
2019-10-24 21:38:13 -0500

    Empty Landing and Deck tests to be finished when classes are.

---
+++ Landing_Test.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Landing_Test {
+
+    @Test
+    void main() {
+    }
+}
--- Shoe.java
+++ Shoe.java
@@ -41,5 +41,4 @@ public class Shoe
         return currentDeck.dealCard();
     }
 
-
 }
