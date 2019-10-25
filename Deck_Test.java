0cac88300114b3e3d83be83693783a516ae4c153
Chad Schadewald <ces83@students.uwf.edu>
2019-10-25 13:52:10 -0500

    Deck_Test on hold until next Sprint.

--- Deck_Test.java
+++ Deck_Test.java
@@ -12,8 +12,32 @@ class Deck_Test
         Card testCard1 = testDeck.dealCard();
         testDeck.Shuffle();
         Card testCard2 = testDeck.dealCard();
+        testDeck.Shuffle();
+        Card testCard3 = testDeck.dealCard();
+        testDeck.Shuffle();
+        Card testCard4 = testDeck.dealCard();
+        testDeck.Shuffle();
+        Card testCard5 = testDeck.dealCard();
 
-        assertNotEquals(testCard1, testCard2);
+        /*
+        System.out.println(testCard1.getRank());
+        System.out.println(testCard2.getRank());
+        System.out.println(testCard3.getRank());
+        System.out.println(testCard4.getRank());
+        System.out.println(testCard5.getRank());
+        System.out.println(testCard1.getSuit());
+        System.out.println(testCard2.getSuit());
+        System.out.println(testCard3.getSuit());
+        System.out.println(testCard4.getSuit());
+        System.out.println(testCard5.getSuit());
+        System.out.println(testCard1.getValue());
+        System.out.println(testCard2.getValue());
+        System.out.println(testCard3.getValue());
+        System.out.println(testCard4.getValue());
+        System.out.println(testCard5.getValue());
+         */
+
+        assertNotEquals(testCard1.getRank(), testCard3.getRank());
     }
     @Test
     void dealCard()
