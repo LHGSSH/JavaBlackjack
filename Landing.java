02edad614f6199a067c081ce38280e00a5fc1dab
Chad Schadewald <ces83@students.uwf.edu>
2019-11-13 01:09:23 -0600

    Messing around with GUI.

--- Landing.java
+++ Landing.java
@@ -1,10 +1,14 @@
 import java.awt.*;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class Landing {
 
@@ -66,27 +70,36 @@ public class Landing {
 			public void actionPerformed(ActionEvent e) 
 			{
 				JFrame popup = new JFrame();
-				popup.setBounds(100, 100, 100, 100);
+				popup.setBounds(100, 100, 950, 750);
 				popup.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				popup.getContentPane().setLayout(new BorderLayout(0, 0));
 				
 				JTextArea txtrCommence = new JTextArea();
 				txtrCommence.setText("Chips are counted...\n Cards are shuffled... \n GOOD LUCK!");
-				popup.getContentPane().add(txtrCommence, BorderLayout.CENTER);
 
-				JPanel testBackground = new JPanel();
-				testBackground.setOpaque(false);
-				testBackground.setLayout(new FlowLayout());
+				JPanel backgroundPanel = new JPanel();
+				backgroundPanel.setOpaque(false);
+				backgroundPanel.setLayout(new BorderLayout());
 
-				JLabel testImage = new JLabel();
-				testImage.setLayout(new FlowLayout());
-				testImage.setIcon(new ImageIcon("Green_Background.png"));
-				testImage.setLayout(new BorderLayout());
+				JLabel backgroundImage = new JLabel();
+				backgroundImage.setIcon(new ImageIcon("Card Images//Green_Background.png"));
+				backgroundImage.setLayout(new BorderLayout());
 
-				testBackground.add(txtrCommence);
+				BufferedImage userCardImage = null;
+				try {
+					userCardImage = ImageIO.read(new File("Card Images//2C.png"));
+				} catch (IOException ex) {
+					ex.printStackTrace();
+				}
+				Image testCard = userCardImage.getScaledInstance(100,150,100);
+				ImageIcon userCardIcon = new ImageIcon(testCard);
+				JLabel testLabel = new JLabel(userCardIcon);
 
-				testImage.add(testBackground);
-				popup.add(testImage);
+				backgroundPanel.add(txtrCommence, BorderLayout.NORTH);
+				backgroundPanel.add(testLabel, BorderLayout.SOUTH);
+
+				backgroundImage.add(backgroundPanel);
+				popup.add(backgroundImage);
 				
 				popup.setVisible(true);
 				
