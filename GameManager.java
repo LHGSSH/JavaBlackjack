7e6d52bf83298838c84fe2068018dcef8d6215f8
Joshua Kleine <jdk31@students.uwf.edu>
2019-11-15 15:28:31 -0600

    Added some Functionality to GameManager

--- GameManager.java
+++ GameManager.java
@@ -8,15 +8,33 @@ public class GameManager {
 
     Shoe s1;
     ChipCount chips;
+    int dealerTotal, userTotal;
+    //The score a new User gets
+    int startingChips = 100;
+    Deck hand;
     //TODO: handle if the shoe is empty before dealing by creating a new shoe
-    public GameManager(User u1){
+    //New game manager is created every time the entire game restarts, not between hands
+    //Landing must allow user to log in (can just let them put in username and password
+    // and make new user if not found in file.
+    public GameManager(String username, String password){
         s1 = new Shoe();
+        User u1 = new User();
+        hand = new Deck(0);
+        if(u1.loginCheck(username,password))
+            chips = new ChipCount(u1.retrieveScore(username, password));
+        else {
+            //Making a new user account and giving default chip count
+            u1 = new User(username, password);
+            chips = new ChipCount(startingChips);
+        }
     }
 
     //Returns the next card, moves to next deck otherwise
-    public Card hit(){
-       //TODO needs logic, this is just so it will compile
-        return s1.deal();
+    public void hit(){
+        if (s1.isShoeEmpty()){
+            s1 = new Shoe();
+        }
+        hand.deckOfCards.add(s1.deal());
     }
     public boolean bet(int betAmount){
         //TODO needs logic, this is just so it will compile
@@ -27,7 +45,31 @@ public class GameManager {
         //TODO needs logic, this is just so it will compile
     }
 
-    public void endGame(){
+    public void endGame(ChipCount chips){
+        //save the chip count to the user score
     }
 
+    public void startGame(){
+        //Deal twice to hand
+    }
+    public void updateHand(){
+        //may interact with landing
+    }
+    public boolean isBust(int total){
+        if (total > 21){
+            return true;
+        }
+        return false;
+    }
+    public boolean isRoundOver(){
+
+        return false;
+    }
+    public void dealerMove(int currentScore){
+        if (currentScore >= 17)
+            //do not draw card\
+            ;
+        else //draw card
+        ;
+    }
 }
