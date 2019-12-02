9665b8a2d4c5a35870b6a3569fa5772272976bb5
LHGSSH <liamhg1@yahoo.com>
2019-12-01 20:16:30 -0600

    Worked on methods inside GameManager.

--- GameManager.java
+++ GameManager.java
@@ -8,10 +8,12 @@ public class GameManager {
 
     Shoe s1;
     ChipCount chips;
-    int dealerTotal, userTotal;
+    int dealerTotal = 0;
+    int userTotal = 0;
     //The score a new User gets
     int startingChips = 100;
-    Deck hand;
+    Deck userHand;
+    Deck dealerHand;
     //TODO: handle if the shoe is empty before dealing by creating a new shoe
     //New game manager is created every time the entire game restarts, not between hands
     //Landing must allow user to log in (can just let them put in username and password
@@ -19,7 +21,8 @@ public class GameManager {
     public GameManager(String username, String password){
         s1 = new Shoe();
         User u1 = new User();
-        hand = new Deck(0);
+        userHand = new Deck(0);
+        dealerHand = new Deck(0);
         if(u1.loginCheck(username,password))
             chips = new ChipCount(u1.retrieveScore(username, password));
         else {
@@ -29,14 +32,19 @@ public class GameManager {
         }
     }
 
-    //Returns the next card, moves to next deck otherwise
+    //Returns the next card and adds its value to the user's total.
+    // Moves to next deck, otherwise.
     
     public Card hit(){
         if (s1.isShoeEmpty()){
             s1 = new Shoe();
         }
-        hand.deckOfCards.add(s1.deal());
-        return hand.dealCard();
+        userHand.deckOfCards.add(s1.deal());
+        //TODO: Add total of dealt card to userTotal
+        //TODO: Add logic for ace card.
+        //If the user gets an ace and if having it as an 11 won't push them over 21,
+        //then set the ace's value as 11. Else, set it as 1.
+        return userHand.dealCard();
     }
     
     public boolean bet(int betAmount){
@@ -45,7 +53,7 @@ public class GameManager {
     }
     
     public void stay(){
-        //TODO needs logic, this is just so it will compile
+        dealerMove(dealerTotal);
     }
 
     public void endGame(ChipCount chips){
@@ -54,12 +62,16 @@ public class GameManager {
 
     public void startGame(){
         //Deal twice to hand
+        dealerMove(dealerTotal);
+        dealerMove(dealerTotal);
+        hit();
+        hit();
     }
     public void updateHand(){
         //may interact with landing
     }
     public boolean isBust(int total){
-        if (total > 21){
+        if (userTotal > 21){
             return true;
         }
         return false;
@@ -68,11 +80,14 @@ public class GameManager {
 
         return false;
     }
-    public void dealerMove(int currentScore){
-        if (currentScore >= 17)
-            //do not draw card\
-            ;
-        else //draw card
-        ;
+    public void dealerMove(int currentScore) {
+        Card dealtCard;
+        if (currentScore < 17) {
+            //Draw card
+            dealtCard = s1.deal();
+            dealerTotal += dealtCard.getValue();
+            //TODO: Add logic for ace card.
+            dealerHand.deckOfCards.add(dealtCard);
+        }
     }
 }
